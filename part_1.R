dd930da33475056be4bb0412a91c700917ae9ae5
ewong027 <ewong027@berkeley.edu>
2015-12-03 12:49:11 -0800

    Code for Monthly Data and Graphics, Also added plots and graphics folder

--- part_1.R
+++ part_1.R
@@ -1,24 +1,45 @@
+#packages needed
+library(stringr)
+library(ggplot2)
+
 # extracting months
-data$month <- as.numeric(str_extract(data$Date, "[0-9]+"))
+data$month <- as.numeric(str_extract(data$date, '[0-9]+'))
 
 # labeling months with words
 month = NULL
 for(i in 1:length(data$month)){     
   word = switch(data$month[i], 
-                "january",
-                "february",
-                "march",
-                "april",
-                "may", 
-                "june", 
-                "july", 
-                "august",
-                "september",
-                "october", 
-                "november",
-                "december"
-  )
+                'January',
+                'February',
+                'March',
+                'April',
+                'May', 
+                'June', 
+                'July', 
+                'August',
+                'September',
+                'October', 
+                'November',
+                'December')
   month = c(month, word)
 }
 
+# Number of Entries Each Month
+month_names <- c('January', 'February', 'March', 'April', 'May', 'June', 
+                 'July', "August", "September", "October", "November",
+                 "December")
 
+monthly_data = NULL
+for (name in month_names){
+  total_per_month <- sum(month == name)
+  monthly_data <- c(monthly_data, total_per_month)
+}
+
+# Graphing the months
+barplot(monthly_data, xlab = 'Months',
+        ylab = 'Number of Accidents',
+        main = 'Number of Crashes Each Month',
+        names.arg = month_names,
+        las = 2,
+        cex.names = .75,
+        col = '#FD075E')
