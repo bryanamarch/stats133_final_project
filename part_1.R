1c96e1fc82dae8126510ec8db745cf27ce93d58b
Lily <ylilyli24@gmail.com>
2015-12-03 10:39:22 -0800

    added sorting by months

--- part_1.R
+++ part_1.R
@@ -1,1 +1,30 @@
-#does this show up?
+# extracting months
+data$month <- as.numeric(str_extract(data$Date, "[0-9]+"))
+
+# labeling months with words
+month = NULL
+for(i in 1:length(data$month)){     
+  word = switch(data$month[i], 
+                "january",
+                "february",
+                "march",
+                "april",
+                "may", 
+                "june", 
+                "july", 
+                "august",
+                "september",
+                "october", 
+                "november",
+                "december"
+  )
+  month = c(month, word)
+}
+
+# exporting decades games csv
+for(i in 1:length(decade_names)){
+  filename = paste(paste("nflweather", decade_names[i], sep = ""), "csv", sep = ".")
+  write.csv(raw_nflweather[raw_nflweather$decade == decade_names[i], col_selected], 
+            file = paste("C:/Users/Lily/Documents/STAT133/HW/cleandata/", filename, sep = ""))
+  i = i+1
+}
