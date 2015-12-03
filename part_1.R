6fb1ec0acfec5fbabaf91f49434aec5a77195a9a
ewong027 <ewong027@berkeley.edu>
2015-12-03 10:44:20 -0800

    removing unnecessary code for this part

--- part_1.R
+++ part_1.R
@@ -21,10 +21,4 @@ for(i in 1:length(data$month)){
   month = c(month, word)
 }
 
-# exporting decades games csv
-for(i in 1:length(decade_names)){
-  filename = paste(paste("nflweather", decade_names[i], sep = ""), "csv", sep = ".")
-  write.csv(raw_nflweather[raw_nflweather$decade == decade_names[i], col_selected], 
-            file = paste("C:/Users/Lily/Documents/STAT133/HW/cleandata/", filename, sep = ""))
-  i = i+1
-}
+
