482c44152ef53ae8e0c6b97f6c831bf7c2699627
Lily <ylilyli24@gmail.com>
2015-12-03 10:42:25 -0800

    labeling by decades

---
+++ part_2.R
@@ -0,0 +1,8 @@
+
+# exporting decades games csv
+for(i in 1:length(decade_names)){
+  filename = paste(paste("nflweather", decade_names[i], sep = ""), "csv", sep = ".")
+  write.csv(raw_nflweather[raw_nflweather$decade == decade_names[i], col_selected], 
+            file = paste("C:/Users/Lily/Documents/STAT133/HW/cleandata/", filename, sep = ""))
+  i = i+1
+}
