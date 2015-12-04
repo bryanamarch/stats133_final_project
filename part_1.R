3883a8b8568e54a0ba4c0ac6fada4818a6b13da6
ewong027 <ewong027@berkeley.edu>
2015-12-04 12:26:21 -0800

    Adding code about how to export the graphics in pdf and png format

--- part_1.R
+++ part_1.R
@@ -42,3 +42,26 @@ barplot(monthly_data, xlab = 'Months',
         las = 2,
         cex.names = .75,
         col = '#FD075E')
+
+# Exporting the Graphics
+#PDF
+pdf('plots_and_graphics/Number_of_Crashes_Each_Month.pdf')
+barplot(monthly_data, xlab = 'Months',
+        ylab = 'Number of Accidents',
+        main = 'Number of Crashes Each Month',
+        names.arg = month_names,
+        las = 2,
+        cex.names = .75,
+        col = '#FD075E')
+dev.off()
+
+#PNG
+png('plots_and_graphics/Number_of_Crashes_Each_Month.png', res = 96)
+barplot(monthly_data, xlab = 'Months',
+        ylab = 'Number of Accidents',
+        main = 'Number of Crashes Each Month',
+        names.arg = month_names,
+        las = 2,
+        cex.names = .75,
+        col = '#FD075E')
+dev.off()
