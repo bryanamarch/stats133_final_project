8296274c6ffd79fef73f36355f544e03083a9e21
bryanamarch <bygutierrez@berkeley.edu>
2015-12-08 10:41:57 -0800

    adding description and cleaning it up

--- code/fun_top3.R
+++ code/fun_top3.R
@@ -1,9 +1,15 @@
+# ======================================================================
+# Function: fun_top3
+# Description: This function subsets the data by decade and extracts 
+#              the three types of panes with the most amount of crashes
+#              in each decade, then concatenates all 30 (10 decades). 
+# ======================================================================
+
 fun_top3 <- function(dat, dec_names) {
   top3 <- NULL
   for (i in dec_names) {
     data_subset <- subset(dat, decade == i)
     value <- head(sort(table(data_subset$type), decreasing = TRUE), n = 3)
-    # name <- paste0('top3_',i)
     top3 <- c(top3, value)
   }
   return(top3)
